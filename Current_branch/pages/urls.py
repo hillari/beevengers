eca116a4eadd427f1ea5aafc7fd8609a7ec78aa1
Rdrichter94 <54921605+Rdrichter94@users.noreply.github.com>
2019-10-17 13:29:30 -0800

    deleted current_branch folder

--- Current_branch/pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('calendar/', views.calendar, name='calendar'),
-]
--- Current_branch/pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def homepage(request):
-   return render(request, 'pages/homepage/index.html')
-def calendar(request):
-   return render(request, 'pages/calendar/calendar.html')
