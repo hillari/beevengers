b02ad8fdfbe2404d8de0067a0245c858cb9c868c
Rareutov <55472914+Rareutov@users.noreply.github.com>
2019-10-17 17:38:52 -0800

    Finished
    I haved added the pages different_bees, education, and recommended_sites. For each of them, I added coralating css and js.

--- django_beevengers/pages/urls.py
+++ django_beevengers/pages/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-
     path('calendar/', views.calendar, name='calendar'),
-
+    path('different_bees/', views.different_bees, name='different_bees'),
+    path('education/', views.education, name='education'),
+    path('recommended_sites/', views.recommended_sites, name='recommended_sites'),
 ]
