13c30b8d65c92a5e3093a80271269acf12cd4026
Rdrichter94 <54921605+Rdrichter94@users.noreply.github.com>
2019-10-18 10:49:16 -0800

    fixed header

--- django_beevengers/pages/urls.py
+++ django_beevengers/pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('homepage/', views.homepage, name='homepage'),
 
     path('calendar/', views.calendar, name='calendar'),
 
