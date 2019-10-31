f2091bb86588d2d876982829f4e179e4ad47f33f
Rdrichter94 <54921605+Rdrichter94@users.noreply.github.com>
2019-10-31 11:44:47 -0800

    more changes

--- django_beevengers/pages/urls.py
+++ django_beevengers/pages/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('homepage/', views.homepage, name='homepage'),
-
-
+    path('', views.redirect_view, name='redirect_view'),
+    path('home/', views.homepage, name='homepage'),
+    path('store/', views.store, name='store'),
     path('calendar/', views.calendar, name='calendar'),
     path('different_bees/', views.different_bees, name='different_bees'),
     path('education/', views.education, name='education'),
