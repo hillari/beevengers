4f503176e84337bf76ae883756f1281ae4d4e8d4
hillari <hillari@gmail.com>
2019-11-17 15:29:22 -0900

    modified base.html and urls.py to use the new class based view for store

--- django_beevengers/store/forms.py
+++ django_beevengers/store/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import *
 
+
 class ProductForm(forms.ModelForm):
 
     class Meta:
--- django_beevengers/store/views.py
+++ django_beevengers/store/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
 
-def product_view(request):
-    return render(request, 'store/product_view/product_view.html')
+class ProductList(generic.ListView):
+    queryset = Product.objects.all()
+    template_name = 'store/product_view/product_list.html'
+
+
+class ProductDetail(generic.DetailView):
+    model = Product
+    template_name = 'store/product_view/product_detail.html'
+
+
+# def model_image_upload(request):
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST, request.FILES)
+
+# def product_view(request):
+#     return render(request, 'store/product_view/product_list.html')
