93e7c360f64364b521043da7261c5bd1118379ff
Rareutov <55472914+Rareutov@users.noreply.github.com>
2019-11-19 18:13:13 -0900

    Changes Some File Names and added slug field
    I changes the product_view name to product_list and made all of the URLs work for the current store pages.
    
    I added a slug field to the product model.
    
    I added a product_detail page that uses a slug in the url to be routed to.

--- django_beevengers/store/models.py
+++ django_beevengers/store/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.text import slugify
+from autoslug import AutoSlugField
+
 import os
 
 
@@ -9,6 +12,8 @@ class Product(models.Model):
     item_name = models.CharField(max_length=200)
     description = models.TextField()
     product_image = models.ImageField(upload_to='images/')
+    slug = AutoSlugField(null=True, default=None, unique=True, populate_from='item_name')
+
     # 6 allows price up to $9,999.99
     product_price = models.DecimalField(default=1.99, max_digits=6, decimal_places=2)
     product_quantity = models.PositiveIntegerField(default=1)
@@ -16,3 +21,5 @@ class Product(models.Model):
     def __str__(self):
         return self.item_name
 
+    def slugify_function(self, content):
+        return slugify(content)
--- django_beevengers/store/urls.py
+++ django_beevengers/store/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductList.as_view(), name='product-list')
-    #path('product_view/', views.product_view, name='product_view'),
+    path('', views.ProductList.as_view(), name='product_list'),
+    path('<slug:slug>/', views.ProductDetail.as_view(), name='product_detail')
+    #path('product_list/', views.product_list, name='product_list'),
 
 ]
