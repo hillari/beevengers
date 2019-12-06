419d1ec2ce7413e6c9c1fe76203217fca0098abb
hillari <hillari@gmail.com>
2019-12-05 21:46:41 -0900

    Adding some exception handling for user test cases

--- django_beevengers/users/tests_users.py
+++ django_beevengers/users/tests_users.py
@@ -13,12 +13,12 @@ class UsersManagersTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
-        # try:
-        #     # username is None for the AbstractUser option
-        #     # username does not exist for the AbstractBaseUser option
-        #     self.assertIsNone(user.username)
-        # except AttributeError:
-        #     pass
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
 
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertRaises
         with self.assertRaises(TypeError):
@@ -35,12 +35,12 @@ class UsersManagersTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
-        # try:
-        #     # username is None for the AbstractUser option
-        #     # username does not exist for the AbstractBaseUser option
-        #     self.assertIsNone(admin_user.username)
-        # except AttributeError:
-        #     pass
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
                 email='super@user.com', password='foo', is_superuser=False)
